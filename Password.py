90c2d5fbf2c3851749a671a31ebbf7331a381225
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-10-29 20:36:30 -0500

    Updated
    Made some changes

--- Password.py
+++ Password.py
@@ -1,16 +1,29 @@
-loop = True
-
 username = input("Please enter a username: ")
 password = input("Please enter a password: ")
 
 passlist = []
 passlist.append(password)
 
-while loop == True:
-    reset = input("Would you like to reset your password? (Y/N) ")
+reset = input("Would you like to reset your password? (Y/N) ")
+try:
+     if reset.lower() == "y":
+          loop = 0
+except:
+     print("Thanks for using my program!")
 
-    if reset.lower == "y":
-        password = input("Please enter a password: ")
-        passlist.append(password)
-    elif reset.lower == "n":
-        loop == False
+while loop == 0:
+    try:
+
+        if reset.lower() == "y":
+            newpass = input("Please enter a password: ")
+            if newpass in passlist:
+                print("That password has been used before!")
+            else:
+                passlist.append(newpass)
+        else:
+            loop += 1
+        if len(passlist) == 4:
+            passlist.pop(0)
+        print(passlist)
+    except:
+        pass
