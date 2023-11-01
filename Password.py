210149017909125d73c4e8b0106e791fb3d9b0df
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-10-31 23:22:16 -0500

    Update Password.py
    Adding more functionality 10/31/2023

--- Password.py
+++ Password.py
@@ -1,29 +1,38 @@
-username = input("Please enter a username: ")
-password = input("Please enter a password: ")
+loop = 0
+secloop = 0
+#Note for future: Will save username and password into a dictionary. Update value (password) associated with key (username)
+while secloop == 0:
+    start = input("Do you have an account? (Y/N) ")
+    try:
+        if start.lower() == "n":
+            username = input("Please enter a username: ")
+            password = input("Please enter a password: ")
+    except:
+        pass
+
+
+#username = input("Please enter a username: ")
+#password = input("Please enter a password: ")
 
 passlist = []
 passlist.append(password)
 
-reset = input("Would you like to reset your password? (Y/N) ")
-try:
-     if reset.lower() == "y":
-          loop = 0
-except:
-     print("Thanks for using my program!")
-
 while loop == 0:
     try:
+        reset = input("Would you like to reset your password? (Y/N) ")
 
         if reset.lower() == "y":
             newpass = input("Please enter a password: ")
             if newpass in passlist:
                 print("That password has been used before!")
+                pass
             else:
                 passlist.append(newpass)
         else:
             loop += 1
         if len(passlist) == 4:
             passlist.pop(0)
-        print(passlist)
     except:
         pass
+
+print("Thanks for using my program!")
