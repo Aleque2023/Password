2bb6eb3878d8d9cd08db7ed1348add0c5e9b5450
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-11-01 23:11:59 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,26 +1,37 @@
 loop = 0
 secloop = 0
+redo = 0
 #Note for future: Will save username and password into a dictionary. Update value (password) associated with key (username)
+
+start = input("Do you have an account? (Y/N) ")
+
 while secloop == 0:
-    start = input("Do you have an account? (Y/N) ")
     try:
         if start.lower() == "n":
             username = input("Please enter a username: ")
-            password = input("Please enter a password: ")
+            password = input("Please enter a password that's 8 characters or more: ")
+            if len(password) < 8:
+                while redo == 0:
+                    print("The password does not meet the requirements!")
+                    password = input("Please enter a password that meets the requirements: ")
+                    try:
+                        if len(password) < 8:
+                            redo == 0
+                        elif len(password) > 7:
+                            redo += 1
+                            secloop += 1
+                    except:
+                        pass
     except:
         pass
-
-
-#username = input("Please enter a username: ")
-#password = input("Please enter a password: ")
 
 passlist = []
 passlist.append(password)
 
+reset = input("Would you like to reset your password? (Y/N) ")
+
 while loop == 0:
     try:
-        reset = input("Would you like to reset your password? (Y/N) ")
-
         if reset.lower() == "y":
             newpass = input("Please enter a password: ")
             if newpass in passlist:
