9b0cbdbcf9876b2951c9d989fbb6afaf94a488fd
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-11-08 23:16:47 -0600

    Making Changes
    Implementing Special Characters requirement for password. (WIP)

--- Password.py
+++ Password.py
@@ -3,6 +3,7 @@ loop = 0
 secloop = 0
 redo = 0
 redo2 = 0
+test = ""
 #Note for future: Will save username and password into a dictionary. Update value (password) associated with key (username)
 
 import time,sys,os
@@ -29,6 +30,8 @@ for char in welcome[6]:
     sys.stdout.flush()
     time.sleep(.01)
 
+special = ["!", "@", "#", "$", "%", "&"]
+
 while start == 0:
 
     start = input("\n\nDo you have an account? (Y/N) ")
@@ -36,45 +39,51 @@ while start == 0:
     while secloop == 0:
         try:
             if start.lower() == "n":
-                username = input("Please enter a username: ")
-                password = input("Please enter a password that's 8 characters or more: ")
-                if len(password) < 8:
-                    while redo == 0:
-                        print("The password does not meet the length requirements!")
-                        password = input("Please enter a password that meets the requirements: ")
-                        try:
-                            if len(password) < 8:
-                                redo == 0
-                            elif len(password) > 7:
-                                redo += 1
-                                secloop += 1
-                        except:
-                            pass
-                else:
-                    secloop += 1
+                username = input("\nPlease enter a username: ")
+                password = input("\nPlease enter a password with the following requirements: \n8 characters or more\nContains a special character\n")
+                for spec in special:
+                    test = spec
+                    if test in password and len(password) > 7:
+                        redo += 1
+                        secloop += 1
+                    else:
+                        while redo == 0:
+                            print("\nThe password does not meet the requirements!")
+                            password = input("\nPlease enter a password that meets the requirements: ")
+                            try:
+                                if len(password) < 8 or test not in password:
+                                    redo == 0
+                                elif len(password) > 7 and test in password:
+                                    redo += 1
+                                    secloop += 1
+                                    exit
+                            except:
+                                pass
+            else:
+                secloop += 1
         except:
             pass
 
     passlist = []
     passlist.append(password)
 
-    reset = input("Would you like to reset your password? (Y/N) ")
+    reset = input("\nWould you like to reset your password? (Y/N) ")
 
     while loop == 0:
         try:
             if reset.lower() == "y":
-                newpass = input("Please enter a password that's 8 characters or more: ")
+                newpass = input("\nPlease enter a password that's 8 characters or more: ")
                 if len(newpass) < 8:
                     redo2 = 0
                     while redo2 == 0:
-                        print("The password does the meet the length requirments!")
-                        newpass = input("Please enter a password that meets the requirements: ")
+                        print("\nThe password does the meet the length requirements!")
+                        newpass = input("\nPlease enter a password that meets the requirements: ")
                         try:
                             if len(newpass) < 8:
                                 redo2 == 0
                             elif len(newpass) > 7:
                                 if newpass in passlist:
-                                    print("That password has been used before!")
+                                    print("\nThat password has been used before!")
                                     redo2 = 0
                                 else:
                                     passlist.append(newpass)
@@ -85,7 +94,7 @@ while start == 0:
                 elif len(newpass) > 7 and newpass not in passlist:
                     loop += 1
                 elif newpass in passlist:
-                    print("That pasword has been used before!")
+                    print("\nThat password has been used before!")
                     loop = 0
             else:
                 loop += 1
@@ -93,14 +102,18 @@ while start == 0:
                 passlist.pop(0)
         except:
             pass
-    question = input("Would you like to run this program again? (Y/N) ")
+    question = input("\nWould you like to run this program again? (Y/N) ")
     try:
         if question.lower() == "y":
             print("\n"*100)
             start = 0
+            loop = 0
+            secloop = 0
+            redo = 0
+            redo2 = 0
         elif question.lower() == "n":
             start += 1
     except:
         pass
 
-print("Thanks for using my program!")
+print("\nThanks for using my program!")
